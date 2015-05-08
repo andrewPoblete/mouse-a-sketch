88eef02da09423b894f6be67eae716d9cf708085
andrewPoblete <andrew_poblete@hotmail.com>
2015-05-07 20:38:12 -0700

    Color working

--- mouse_a_sketch/base.js
+++ mouse_a_sketch/base.js
@@ -2,4 +2,21 @@ console.log("this is a sanity check");
 
 $(document).ready(function(){
   console.log("Let's get coding!")
+  	
+  	var atd = $("td");
+
+  	// for(var i = 0; i < atd.length; i++) {
+  	// 	$(atd[i]).on("mouseover", colorMe);  		
+  	// }
+
+  	$.each($(atd), function(i, v){
+  		$(atd[i]).on("click", colorMe);
+  	});
+
+  	function colorMe() {
+  		var color = $("input").val();
+
+  		$(this).css("background-color", color);
+  	}
+  		
 })
